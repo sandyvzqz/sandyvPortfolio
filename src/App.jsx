7d9d91924c2ca20652ba9bf184b51bbc3db68f8d
Husky Lover <ssolivares0@gmail.com>
2024-06-26 11:46:03 -0400

    Made project component, added images and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navigation from './components/Navigation';
 // import Header from './components/Header';
-// import Project from './components/Project';
+import Project from './components/Project';
 import Footer from './components/Footer';
 import './App.css'
 
@@ -10,7 +10,7 @@ function App() {
       < Navigation />
       {/* < Header /> */}
       {/* project will need a nested project card component */}
-      {/* < Project /> */}
+      < Project />
       < Footer />
     </div>
   )
