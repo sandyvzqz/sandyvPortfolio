e4e6637acffee1759d87c0df5f67e8ebd8e4f38a
Husky Lover <ssolivares0@gmail.com>
2024-07-01 23:36:18 -0400

    updated stackoverflow profile link.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = ()=> {
                         <div className="social-icon">
                             <a href="https://github.com/sandyvzqz" target="_blank"><img src={github} alt="github logo"/></a>
                             <a href="https://www.linkedin.com/in/sandy-vazquez-847a1a316/" target="_blank"><img src={linkedIn} alt="linkedIn logo"/></a>
-                            <a href="https://stackoverflow.com" target="_blank"><img src={stackOverflow} alt="stackOverflow logo"/></a>
+                            <a href="https://stackexchange.com/users/33479727/sandy-v18" target="_blank"><img src={stackOverflow} alt="stackOverflow logo"/></a>
                         </div>
                     </Col>
                 </Row>
