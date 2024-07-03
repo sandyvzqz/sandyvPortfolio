b8b125e428a656c3bead10abaa6870cb202fa6ca
Husky Lover <ssolivares0@gmail.com>
2024-07-03 17:27:52 -0400

    Wrapped resume content id in new div tag instead of section tag. The id in section tag was causing errors in other components of the app.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -17,40 +17,42 @@ const Resume = () => {
         });
     };
     return(
-        <section id="resumeContent">
-            <h1>Resume</h1>
-            <div>
-                <h3>Resume</h3>
-                <div className="resumeContact">
-                    <li>Sandy Vazquez</li>
-                    <li>(919)810-5588</li>
-                    <li>ssolivares0@gmail.com</li>
+        <section>
+            <div id="resumeContent">
+                <h1>Resume</h1>
+                <div>
+                    <h3>Resume</h3>
+                    <div className="resumeContact">
+                        <li>Sandy Vazquez</li>
+                        <li>(919)810-5588</li>
+                        <li>ssolivares0@gmail.com</li>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <h3> Professional Experience</h3>
-                <p> As an aspiring web developer, I do not have any professional working experience yet.</p>
-            </div>
-            <div>
-                <h3> Career Objective</h3>
-                <p>Aspiring web developer looking for an opportunity to work professionally with a team that will allow me room to grow into a more seasoned web developer.</p>
-            </div>
-            <div>
-                <h3>Education</h3>
-                <li>UNC Chapel Hill Coding Bootcamp</li>
-                <li>January - July 2024</li>
-            </div>
+                <div>
+                    <h3> Professional Experience</h3>
+                    <p> As an aspiring web developer, I do not have any professional working experience yet.</p>
+                </div>
+                <div>
+                    <h3> Career Objective</h3>
+                    <p>Aspiring web developer looking for an opportunity to work professionally with a team that will allow me room to grow into a more seasoned web developer.</p>
+                </div>
+                <div>
+                    <h3>Education</h3>
+                    <li>UNC Chapel Hill Coding Bootcamp</li>
+                    <li>January - July 2024</li>
+                </div>
 
-            <div>
-                <h3> Skills</h3>
-                <li>CSS/HTML</li>
-                <li>JavaScript</li>
-                <li>SQL</li>
-                <li>REST APIs</li>
-                <li>Mongo DB</li>
-                <li>React </li>
+                <div>
+                    <h3> Skills</h3>
+                    <li>CSS/HTML</li>
+                    <li>JavaScript</li>
+                    <li>SQL</li>
+                    <li>REST APIs</li>
+                    <li>Mongo DB</li>
+                    <li>React </li>
+                </div>
+                <button onClick={ downloadPDF}>Download Resume</button>
             </div>
-            <button onClick={ downloadPDF}>Download Resume</button>
         </section>
     )
 }
